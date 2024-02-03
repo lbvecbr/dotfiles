68bb5e3658e4953693ea095d52871b921de53264
dimon <ivdmgen@gmail.com>
2024-02-03 15:29:55 +0200

    New config VimScript based for Neovim, installed colorschemas and better-escape

---
+++ nvim.lua.purple/init.lua
@@ -0,0 +1,24 @@
+-- Basic config
+require('core.plugins')
+require('core.colors')
+require('core.configs')
+require('core.mappings')
+
+
+
+
+
+-- Plugins config
+require('plugins.treesitter')
+require('plugins.cmp')
+require('plugins.lsp')
+require('plugins.mason')
+require('plugins.neodev')
+require('plugins.neotree')
+require('plugins.toggleterm')
+require('plugins.autopairs')
+require('plugins.autotag')
+require('plugins.buferline')
+require('plugins.telescope')
+require('plugins.null_ls')
+
